fc106bed4171a1ee18a4a441dfe019aa23a53672
minnkoaung <minnkoaung@gmail.com>
2018-01-10 11:51:55 +0630

    contact page added

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,15 +3,98 @@
 @section('title')
     <title>May Myanmar | Contact Us</title>
 @endsection
-
 @section('content')
-	<div class="container-fluid maincontents maincontent_fluid">
-	   <div class="container test">
-	   	<div class="jumbotron" style="margin-top: 30px;">
-		  <h1>Hello!, This is <span class="text-danger">Contact Page</span> but ...</h1>
-		  <p> This webpage is not setup yet. Sorry. </p>
-		  <p><a class="btn btn-primary btn-lg" href="/" role="button">Go back to Home Page</a></p>
+	<!-- page navigator start -->
+	<div class="container-fluid page_navigator_fluid">
+		<div class="container page_navigator_container">
+			<div class="row page_navigator_row">
+				<div class="page_navigator_col">
+					<ol class="breadcrumb col-md-12">
+						<li><a href="\">Home</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
+						<li><a href="#">Contact Us</a></li>
+					</ol>
+				</div>
+			</div>
 		</div>
+	</div>
+	<!-- page navigator end -->
+	
+	<div class="container-fluid maincontents maincontent_fluid">
+	   <!-- contactus_container start -->
+	   <div class="container contactus_container">
+	   		<!-- contactus_row #1 start -->
+			<div class="row contactus_row">
+				<div class="col-md-12 content_title_col contactus_title_col">
+	   				<h2 class="text-uppercase content_title contactus_title" ><strong>Contact Us</strong> </h2>
+	   			</div>
+	   			<div class="col-md-6 col-sm-6 col-xs-12">
+	   				<p class="">
+	   					<strong class="text-blue" style="display:inline-block;font-size: 1.2em;padding: 5px 0 5px 0;">Bukit Timah Branch</strong><br><br>
+	   					170 Upper Bukit Timah Rd, #01-43 Bukit Timah Shopping Centre,<br> Singapore 588179 <br><br>
+	   					6891 2252 / 6891 2262 <br><br>
+	   					<a href="">maymyanmarservices@gmail.com </a>
+	   				</p>
+	   				<div class="row">
+	   					<div class="col-md-12">
+	   						<iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=103.77420544624329%2C1.342046610151584%2C103.77774596214294%2C1.3445162457770832&amp;layer=mapnik&amp;marker=1.343280087540402%2C103.77597570419312" style="border: 1px solid black"></iframe>
+	   					</div>
+	   				</div>
+	   			</div>
+	   			<div class="col-md-6 col-sm-6 col-xs-12">
+	   				<p class="">
+	   					<strong class="text-blue" style="display:inline-block;font-size: 1.2em;padding: 5px 0 5px 0;">Katong Branch</strong><br><br>
+	   					865 Mountbatten Rd, #02-70/71/72 Katong Shopping Centre,<br> Singapore 437844 <br><br>
+	   					6440 5354 / 6440 4454 / 6440 4456  <br><br>
+	   					<a href="">khinzaw@maymyanmar.com.sg</a>
+	   				</p>
+	   				<div class="row">
+	   					<div class="col-md-12">
+	   						<iframe width="100%" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=103.89670193195344%2C1.3013656443933408%2C103.90566051006319%2C1.3063049942111513&amp;layer=mapnik&amp;marker=1.3038326389991166%2C103.9011812210083" style="border: 1px solid black"></iframe><br/>
+	   					</div>
+	   				</div>
+	   			</div>
+			</div>
+			<!-- contactus_row #1 end -->
+			<br><br>
+			<!-- contactus_row #2 start -->
+			<div class="row contactus_row">
+				<div class="col-md-12 content_title_col contactus_title_col">
+	   				<h4 class="text-uppercase content_title contactus_title text-blue" ><strong>Get In Touch</strong> </h4>
+	   			</div>
+	   			<div class="col-md-12">
+	   				<form action="" method="" class="contact_form">
+					   <div class="form-group contact_form_group">
+					    <input type="text" class="form-control" id="name" placeholder="Your Name" >
+					  </div>
+					  <div class="form-group contact_form_group">
+					    <input type="email" class="form-control" id="email" placeholder="Email">
+					  </div>
+					  <div class="form-group contact_form_group">
+					    <textarea class="form-control" rows="10"></textarea>
+					  </div>
+  						<a href="" class="btn view_detail_btn">Send Message</a>  
+					</form>
+	   			</div>
+			</div>
+			<!-- contactus_row #2 end -->
+			
+			
+			
+			<!-- training_row #2 start -->
+			<div class="row testimonial_row">
+				<div class="col-md-12 testimonial_inside_col">
+					<div class="row testimonial_inside_row">
+						<div class="col-md-12 ojt testimonial">
+							<img src="{{asset('images/training/training_15.png')}}" class="pull-right img-responsive" style="" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- training_row #3 end -->
+			@include('frontends.partner')
 	   </div>
+	    <!-- training_container end -->
 	</div>
 @endsection
+
--- resources/views/pages/joinus.blade.php
+++ resources/views/pages/joinus.blade.php
@@ -3,15 +3,94 @@
 @section('title')
     <title>May Myanmar | Join Us</title>
 @endsection
-
 @section('content')
-	<div class="container-fluid maincontents maincontent_fluid">
-	   <div class="container test">
-	   	<div class="jumbotron" style="margin-top: 30px;">
-		  <h1>Hello!, This is <span class="text-danger">Join Us</span> Page but ...</h1>
-		  <p> This webpage is not setup yet. Sorry. </p>
-		  <p><a class="btn btn-primary btn-lg" href="/" role="button">Go back to Home Page</a></p>
+	<!-- page navigator start -->
+	<div class="container-fluid page_navigator_fluid">
+		<div class="container page_navigator_container">
+			<div class="row page_navigator_row">
+				<div class="page_navigator_col">
+					<ol class="breadcrumb col-md-12">
+						<li><a href="\">Home</a></li>
+						<li><i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
+						<li><a href="#">Join Us</a></li>
+					</ol>
+				</div>
+			</div>
 		</div>
+	</div>
+	<!-- page navigator end -->
+	
+	<div class="container-fluid maincontents maincontent_fluid">
+	   <!-- joinus_container start -->
+	   <div class="container joinus_container">
+	   		<!-- joinus_row #1 start -->
+			<div class="row joinus_row">
+				<div class="col-md-12 content_title_col joinus_title_col">
+	   				<h2 class="text-uppercase content_title joinus_title" ><strong>Job Seekers</strong> </h2>
+	   			</div>
+	   			<div class="col-md-12 col-sm-12 col-xs-12">
+	   				<p class="text-justify">
+	   					We are always on the lookout for professionals to join our teams across the region. If you are interested in pursuing a career with us, please send us an email and submit it to jeleel@maymyanmar.com.sg, enclosing your full curriculum vitae and a recent photograph together with details of your current remuneration, expected salary and contact telephone number. <br><br><br>
+	   					<strong class="text-blue" style="display:inline-block;font-size: 1.2em;padding: 5px 0 5px 0;">Salary will commensurate with experience and qualifications.</strong><br>
+	   					(Only shortlisted candidates will be notified)<br><br>
+	   					<strong class="text-blue" style="display:inline-block;font-size: 1.2em;padding: 5px 0 5px 0;">At May Manpower, we specialise in recruitment services for the following job positions:</strong><br>
+	   				</p>
+	   				<div class="row">
+	   					<div class="col-md-3">
+	   						<div class="row">
+	   							<div class="col-md-12 text-center">
+	   								<img src="{{asset('images/joinus/joinus_03.png')}}" class="img-responsive" style="margin: 0px auto;">
+	   							</div>
+	   							<div class="col-md-12 text-center">
+	   								<h5 >Human Resources Jobs</h5>
+	   							</div>
+	   						</div>
+	   					</div>
+
+	   					<div class="col-md-3">
+	   						<div class="row">
+	   							<div class="col-md-12 text-center">
+	   								<img src="{{asset('images/joinus/joinus_03.png')}}" class="img-responsive" style="margin: 0px auto;">
+	   							</div>
+	   							<div class="col-md-12 text-center">
+	   								<h5 >Human Resources Jobs</h5>
+	   							</div>
+	   						</div>
+	   					</div>
+
+	   					<div class="col-md-3">
+	   						<div class="row">
+	   							<div class="col-md-12 text-center">
+	   								<img src="{{asset('images/joinus/joinus_07.png')}}" class="img-responsive" style="margin: 0px auto;">
+	   							</div>
+	   							<div class="col-md-12 text-center">
+	   								<h5 >Human Resources Jobs</h5>
+	   							</div>
+	   						</div>
+	   					</div>
+
+	   					<div class="col-md-3">
+	   						<div class="row">
+	   							<div class="col-md-12 text-center">
+	   								<img src="{{asset('images/joinus/joinus_09.png')}}" class="img-responsive" style="margin: 0px auto;">
+	   							</div>
+	   							<div class="col-md-12 text-center">
+	   								<h5 >Human Resources Jobs</h5>
+	   							</div>
+	   						</div>
+	   					</div>
+	   				</div>
+	   			</div>
+			</div>
+			<!-- joinus_row #1 end -->
 	   </div>
+	    <!-- joinus_container end -->
+	</div>
+	<div class="container-fluid faq_full_img_container" style="margin-top: 150px;"></div>
+	<div class="container-fluid partner_container_fluid">
+		<div class="container">
+			@include('frontends.partner')
+		</div>
 	</div>
 @endsection
+
